b3a0fa3aee4aabed1eb1f00b229dd450bf25c97f
Nazeer <nazeerazam45@gmail.com>
2025-10-18 21:20:29 +0530

    different routes and database connection

---
+++ blog/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# check_same_thread = False is required for sqlite database to work with FastAPI in a multi-threaded environment
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#sessionmaker is a function that generates database sessions
+#autocommit=False means that the database will not commit the changes to the database automatically
+#autoflush=False means Prevents automatic flushing of objects to the database
+#bind=engine means that the session will use the engine to connect to the database
+
+Base = declarative_base()
+# Base class for all database models, every model inherits from it
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,3 +11,29 @@ def index():
 @app.get("/about")
 def about():
     return {"message": "About page"}
+
+@app.get("/blog/{id}")
+def blog(id: int):
+    return {"id": id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
+
+@app.get('/blog')
+def index(limit: int = 10, published: bool = False, sort: Optional[str] = None):
+    # only get {limit} number of blogs
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+    
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog is created with title as {blog.title}'}
